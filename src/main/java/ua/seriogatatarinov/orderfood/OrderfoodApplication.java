44d0ea41a2b094e670de0ed9c83b0ec8548a6b04
kurson2234562 <kurson.aaa@yandex.ua>
2018-05-09 21:30:10 +0300

    Initial commit

---
+++ src/main/java/ua/seriogatatarinov/orderfood/OrderfoodApplication.java
@@ -0,0 +1,12 @@
+package ua.seriogatatarinov.orderfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderfoodApplication.class, args);
+	}
+}
